205715b9d8b31db1e7d349d0ae51013428e3ad8a
Emilio Said <saidmacciseemilio@gmail.com>
2022-11-23 16:04:18 -0600

    App finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample )
+  rating = Review.new(rating: rand(0..5), content: Faker::Restaurant.review)
+  rating.restaurant = restaurant
+  rating.save
+end
